2135fb5a1881a1a763417e77a7248f4e365269d9
Piotr Oleś <piotr.oles@datadoghq.com>
2022-06-26 22:23:00 +0200

    fix: improve reliability for big .proto files
    Use fs-extra and async function to load .proto file. Thanks to @Seb-figment for fixing this.

--- packages/as-proto-gen/src/index.ts
+++ packages/as-proto-gen/src/index.ts
@@ -7,54 +7,63 @@ import { generateFile } from "./generate/file";
 import { getPathWithoutProto } from "./names";
 import { FileContext } from "./file-context";
 import prettier from "prettier";
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as assert from "assert";
 
-const input = fs.readFileSync(process.stdin.fd);
-
-try {
-  const codeGenRequest = CodeGeneratorRequest.deserializeBinary(input);
-  const codeGenResponse = new CodeGeneratorResponse();
-  const generatorContext = new GeneratorContext();
-
-  codeGenResponse.setSupportedFeatures(
-    CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL
-  );
-
-  for (const fileDescriptor of codeGenRequest.getProtoFileList()) {
-    const fileDescriptorName = fileDescriptor.getName();
-    assert.ok(fileDescriptorName);
-
-    generatorContext.registerFile(fileDescriptor);
-  }
-
-  for (const fileName of codeGenRequest.getFileToGenerateList()) {
-    const fileDescriptor =
-      generatorContext.getFileDescriptorByFileName(fileName);
-    assert.ok(fileDescriptor);
-
-    const generatedCode = generateFile(
-      fileDescriptor,
-      new FileContext(generatorContext, fileDescriptor)
-    );
-    let formattedCode = generatedCode;
-    try {
-      formattedCode = prettier.format(generatedCode, {
-        parser: "typescript",
-      });
-    } catch (error) {
-      console.error(error);
-    }
-
-    const outputFile = new CodeGeneratorResponse.File();
-    outputFile.setName(getPathWithoutProto(fileName) + ".ts");
-    outputFile.setContent(formattedCode);
-    codeGenResponse.addFile(outputFile);
-  }
-
-  process.stdout.write(Buffer.from(codeGenResponse.serializeBinary().buffer));
-} catch (error) {
+const reportErrorAndExit = (error: unknown) => {
   console.log("An error occurred in as-proto generator plugin.");
   console.error(error);
   process.exit(1);
 }
+
+fs.readFile(process.stdin.fd, (error, input) => {
+  if (error) {
+    reportErrorAndExit(error);
+    return;
+  }
+
+  try {
+    const codeGenRequest = CodeGeneratorRequest.deserializeBinary(input);
+    const codeGenResponse = new CodeGeneratorResponse();
+    const generatorContext = new GeneratorContext();
+
+    codeGenResponse.setSupportedFeatures(
+        CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL
+    );
+
+    for (const fileDescriptor of codeGenRequest.getProtoFileList()) {
+      const fileDescriptorName = fileDescriptor.getName();
+      assert.ok(fileDescriptorName);
+
+      generatorContext.registerFile(fileDescriptor);
+    }
+
+    for (const fileName of codeGenRequest.getFileToGenerateList()) {
+      const fileDescriptor =
+          generatorContext.getFileDescriptorByFileName(fileName);
+      assert.ok(fileDescriptor);
+
+      const generatedCode = generateFile(
+          fileDescriptor,
+          new FileContext(generatorContext, fileDescriptor)
+      );
+      let formattedCode = generatedCode;
+      try {
+        formattedCode = prettier.format(generatedCode, {
+          parser: "typescript",
+        });
+      } catch (error) {
+        console.error(error);
+      }
+
+      const outputFile = new CodeGeneratorResponse.File();
+      outputFile.setName(getPathWithoutProto(fileName) + ".ts");
+      outputFile.setContent(formattedCode);
+      codeGenResponse.addFile(outputFile);
+    }
+
+    process.stdout.write(Buffer.from(codeGenResponse.serializeBinary().buffer));
+  } catch (error) {
+    reportErrorAndExit(error);
+  }
+})
